c6317d4f2c1cd4ab362e0464682fdb93345f60e1
Terry Chia <chiawz@hotmail.com>
2023-04-16 19:47:49 +0800

    feat[NK-118]: update pipeline for pytest coverage testing

---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+from src.app import app
+
+def test_index_route():
+    response = app.test_client().get('/')
+
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') != ''

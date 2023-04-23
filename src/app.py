c3048bae51232800ef9e111e78b53d4e4b356334
gitkh2891 <123794724+gitkh2891@users.noreply.github.com>
2023-04-23 21:37:25 +0800

    replaced app.py with new routes for "/" and "/main"

--- src/app.py
+++ src/app.py
@@ -1,10 +1,34 @@
-from flask import Flask, send_from_directory, request, abort
+#### App.py code
 
-app = Flask(__name__)
+from flask import Flask, flash, render_template, Response, request, redirect, url_for
 
-@app.route('/')
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+
+@app.route("/")
 def index():
-    return send_from_directory('Views', 'index.html')
+    return render_template('login-page.html', ovalue=0, message ="")
 
-if __name__ == '__main__':
-    app.run(host="localhost",port=8080)
+
+@app.route("/main", methods=["POST"])
+def success():    
+    
+    #placeholder block for handling the HTTP POST request after form submission 
+    name = request.form.get('username')    
+    pw = request.form.get('password')     
+    # print(name) - for debugging
+    # print(pw) - for debugging
+
+    # placeholder logic to implement authentication checks later
+
+    if (pw == "admin"):
+        return render_template('index-placeholder.html')
+        #return render_template('main-page.html') - for debugging
+        #render the main page if auth checks are ok
+    else:
+        return render_template('login-page.html', ovalue=1.0, message="Login error: wrong username and/or password")
+         #render the login page again with error message
+        
+
+if __name__=='__main__':
+    app.run(host="localhost",port=3000, debug = True)
